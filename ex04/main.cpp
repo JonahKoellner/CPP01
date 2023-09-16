5f53e08a820daa0cd7cc180dff384f1fcec9ca5d
Jonah Köllner <koellnjo@gmail.com>
2023-09-16 11:28:53 +0200

    finished and it works like a charm. Also i think its a pretty good solution

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jonahkollner <jonahkollner@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 11:06:39 by jonahkollne       #+#    #+#             */
-/*   Updated: 2023/09/16 11:13:56 by jonahkollne      ###   ########.fr       */
+/*   Updated: 2023/09/16 11:28:18 by jonahkollne      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,27 @@ int main(int argc, char *argv[]){
 	if (argc != 4)
 		return (std::cout << "Input error! (" << argv[0] << " <file_name> <find> <replace>)" << std::endl, 1);
 
-	// Read file
+	// Open file
 	std::string file_name(argv[1]);
 	std::ifstream file(file_name);
 	if (file.fail())
 		return (std::cout << file_name << " <--- File not found! (" << argv[0] << " <file_name> <find> <replace>)" << std::endl, 1);
 
+	// Read file content and store
 	std::stringstream file_content;
 	file_content << file.rdbuf();
+	std::string content (file_content.str());
 	file.close();
 
 	// Replace algorithm
 	std::string to_replace(argv[2]);
 	std::string replacement(argv[3]);
-	std::string::size_type found_pos = file_content.str().find(to_replace);
+	std::string::size_type found_pos = content.find(to_replace);
 	while (found_pos != std::string::npos){
-		file_content.str().erase(found_pos, to_replace.length());
-		file_content.str().insert(found_pos, replacement);
-		std::cout << file_content.str();
-		found_pos = file_content.str().find(to_replace);
+		content.erase(found_pos, to_replace.length());
+		content.insert(found_pos, replacement);
+		std::cout << content;
+		found_pos = content.find(to_replace);
 	}
 
 	// Replace ending for file name
@@ -49,7 +51,7 @@ int main(int argc, char *argv[]){
 
 	// Fille replaced content
 	std::ofstream file_replace (replace_file_name);
-	file_replace << file_content.str();
+	file_replace << content;
 	file_replace.close();
 	return (0);
 }
